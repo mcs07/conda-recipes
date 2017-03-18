91667be7cf6e07259fb8b54dbd4fb5e75cc29674
Matt Swain <m.swain@me.com>
2017-03-18 14:00:03 +0000

    leptonica: Help windows find jpeg

--- recipes/leptonica/bld.bat
+++ recipes/leptonica/bld.bat
@@ -3,13 +3,14 @@ cd build
 cmake -G "%CMAKE_GENERATOR%" ^
       -D BUILD_PROG=1 ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D JPEG_LIBRARY=%LIBRARY_LIB%\jpeg.lib ^
+      -D JPEG_INCLUDE_DIR=%LIBRARY_INC% ^
       -D TIFF_LIBRARY=%LIBRARY_LIB%\libtiff.lib ^
       -D TIFF_INCLUDE_DIR=%LIBRARY_INC% ^
       -D PNG_LIBRARY=%LIBRARY_LIB%\libpng.lib ^
       -D PNG_PNG_INCLUDE_DIR=%LIBRARY_INC% ^
       -D ZLIB_LIBRARY=%LIBRARY_LIB%\zlib.lib ^
       -D ZLIB_INCLUDE_DIR=%LIBRARY_INC% ^
-      -D CMAKE_BUILD_TYPE=Release ^
       ..
 if errorlevel 1 exit 1
 
