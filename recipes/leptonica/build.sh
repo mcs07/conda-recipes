e6eaa8290fbb8e4323fe798a08bc7990c4255307
Matt Swain <m.swain@me.com>
2017-03-18 00:39:11 +0000

    leptonica: Try remove explicit options

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./autobuild
-LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib" CFLAGS="-I$PREFIX/include" ./configure --prefix=$PREFIX --with-libpng --with-jpeg --with-libopenjpeg --with-libtiff
+LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib" CFLAGS="-I$PREFIX/include" ./configure --prefix=$PREFIX
 make
 make install
