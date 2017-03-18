1953962feb323f4bdc63b77b2109b3dd7dc01499
Matt Swain <m.swain@me.com>
2017-03-18 13:59:52 +0000

    leptonica: Help osx find dependencies

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./autobuild
-LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib" CFLAGS="-I$PREFIX/include" ./configure --prefix=$PREFIX
+LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib" CFLAGS="-I$PREFIX/include" sh ./configure --prefix=$PREFIX
 make
 make install
