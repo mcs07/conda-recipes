77b5535f443e3e63f0eed9af41ed914211470b41
Matt Swain <m.swain@me.com>
2017-03-18 15:08:23 +0000

    leptonica: Try switch back to release

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 export LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib"
 export CFLAGS="-I$PREFIX/include"
-./autobuild
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX  --with-libpng --with-jpeg --with-libopenjpeg --with-libtiff
 make
 make install
