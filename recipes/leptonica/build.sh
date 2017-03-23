2e658cbfccbc449b5c31e4c8a19bd072d68f3d32
Matt Swain <m.swain@me.com>
2017-03-23 20:33:24 +0000

    leptonica: Fix linux

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 export CFLAGS="-g -O2 -I$PREFIX/include"
 export LDFLAGS="-Wl,-rpath -Wl,-L$PREFIX/lib"
-./autobuild
 ./configure --prefix=$PREFIX --with-libpng --with-jpeg --with-libopenjpeg --with-libtiff
 make
 make install
