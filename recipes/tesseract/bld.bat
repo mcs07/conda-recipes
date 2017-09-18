c8c59194fa0205a51dc9829274ed0fef2540007e
Matt Swain <m.swain@me.com>
2017-09-18 11:48:22 +0100

    tesseract: Use Release config during install also

--- recipes/tesseract/bld.bat
+++ recipes/tesseract/bld.bat
@@ -13,5 +13,5 @@ if errorlevel 1 exit 1
 cmake --build . --config Release
 if errorlevel 1 exit 1
 
-cmake --build . --target install 
+cmake --build . --config Release --target install 
 if errorlevel 1 exit 1
