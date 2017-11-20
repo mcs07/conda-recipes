8299a7fd3a0b02532b3ac3db414fe3ca3b30bc3f
Matt Swain <m.swain@vernalis.com>
2017-11-20 15:46:04 +0000

    tesseract: Put tessdata in build prefix also

--- recipes/tesseract/bld.bat
+++ recipes/tesseract/bld.bat
@@ -19,7 +19,12 @@ if errorlevel 1 exit 1
 :: Make copies of the .lib file without the embedded version number
 copy %LIBRARY_LIB%\tesseract305.lib %LIBRARY_LIB%\tesseract.lib
 
+:: Download tessdata to bin directory so tesseract executable works
 mkdir %LIBRARY_BIN%\tessdata
 cd %LIBRARY_BIN%\tessdata
 curl -L -O "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/eng.traineddata"
 curl -L -O "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/osd.traineddata"
+
+:: Copy tessdata to build prefix so tesseract API works
+mkdir %PREFIX%\tessdata
+copy %LIBRARY_BIN%\tessdata\*.traineddata %PREFIX%\tessdata\
