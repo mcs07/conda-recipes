0f995a2fbbf8ea7942ba568086c0af47c908ab9c
Matt Swain <m.swain@me.com>
2018-05-11 13:27:53 +0100

    CI: Fix python syntax error

--- .ci_support/build_all.py
+++ .ci_support/build_all.py
@@ -47,7 +47,7 @@ def build_all(recipes_dir, arch):
 
     config = conda_build.api.Config(
         variant_config_files=variant_config_files, arch=arch,
-        exclusive_config_file=exclusive_config_file, channel_urls=channel_urls
+        exclusive_config_file=exclusive_config_file, channel_urls=channel_urls,
         token=os.environ.get('BINSTAR_TOKEN'), user='mcs07')
 
     worker = {'platform': platform, 'arch': arch,
