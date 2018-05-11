31f3bd92022819a17f011581259a2b201d66238b
Matt Swain <m.swain@me.com>
2018-05-11 13:24:13 +0100

    CI: Update for conda-build 3

--- .circleci/run_docker_build.sh
+++ .circleci/run_docker_build.sh
@@ -6,41 +6,66 @@ IMAGE_NAME="condaforge/linux-anvil"
 docker info
 
 config=$(cat <<CONDARC
+
 channels:
  - mcs07
  - conda-forge
  - defaults
+
 conda-build:
  root-dir: /home/conda/conda-recipes/build_artifacts
+
 always_yes: true
 show_channel_urls: true
+
 CONDARC
 )
+
+# In order for the conda-build process in the container to write to the mounted
+# volumes, we need to run with the same id as the host machine, which is
+# normally the owner of the mounted volumes, or at least has write permission
+HOST_USER_ID=$(id -u)
+# Check if docker-machine is being used (normally on OSX) and get the uid from
+# the VM
+if hash docker-machine 2> /dev/null && docker-machine active > /dev/null; then
+    HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
+fi
 
 rm -f "$REPO_ROOT/build_artefacts/conda-forge-build-done"
 
 cat << EOF | docker run -i \
-                        -v "${REPO_ROOT}":/conda-recipes \
+                        -v ${REPO_ROOT}:/home/conda/conda-recipes \
                         -a stdin -a stdout -a stderr \
+                        -e HOST_USER_ID=${HOST_USER_ID} \
                         $IMAGE_NAME \
                         bash -ex || exit $?
+
 if [ "${BINSTAR_TOKEN}" ];then
     export BINSTAR_TOKEN=${BINSTAR_TOKEN}
 fi
+
+# Copy the host recipes folder so we don't ever muck with it
+cp -r /home/conda/conda-recipes/recipes ~/conda-recipes
+cp -r /home/conda/conda-recipes/.ci_support ~/.ci_support
+
 # Unused, but needed by conda-build currently... :(
 export CONDA_NPY='19'
+
 echo "$config" > ~/.condarc
+
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artifacts.
 conda clean --lock
-conda update conda conda-build
-conda install conda-forge-build-setup
+
+conda install conda-forge-ci-setup=1.* conda-forge-pinning networkx anaconda-client
 source run_conda_forge_build_setup
-conda install conda-build-all anaconda-client=1.6.5
+
 # yum installs anything from a "yum_requirements.txt" file that isn't a blank line or comment.
-find /conda-recipes -mindepth 2 -maxdepth 2 -type f -name "yum_requirements.txt" \
+find ~/conda-recipes -mindepth 2 -maxdepth 2 -type f -name "yum_requirements.txt" \
     | xargs -n1 cat | grep -v -e "^#" -e "^$" | \
     xargs -r /usr/bin/sudo -n yum install -y
+
 conda info
 conda config --get
-conda build-all /conda-recipes/recipes --inspect-channels mcs07 --upload-channels mcs07 --matrix-conditions "numpy >=1.11" "python >=2.7,<3|>=3.5" "r-base >=3.3.2"
+
+python ~/.ci_support/build_all.py ~/conda-recipes
 EOF
