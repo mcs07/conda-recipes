7bb953eedf85f8712ab7e3ba9df86ae363d70e4e
Matt Swain <m.swain@me.com>
2018-05-11 15:02:03 +0100

    CI: Make sure we skip existing

--- .ci_support/build_all.py
+++ .ci_support/build_all.py
@@ -48,7 +48,7 @@ def build_all(recipes_dir, arch):
     config = conda_build.api.Config(
         variant_config_files=variant_config_files, arch=arch,
         exclusive_config_file=exclusive_config_file, channel_urls=channel_urls,
-        token=os.environ.get('BINSTAR_TOKEN'), user='mcs07')
+        token=os.environ.get('BINSTAR_TOKEN'), user='mcs07', skip_existing=True)
 
     worker = {'platform': platform, 'arch': arch,
               'label': '{}-{}'.format(platform, arch)}
