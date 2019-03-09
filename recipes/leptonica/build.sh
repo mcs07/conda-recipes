fb8ec840e09fa8271e22a29b99633de9e041b059
Matt Swain <m.swain@me.com>
2019-03-09 19:46:38 +0000

    leptonica: Update to v1.77.0

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-# This is a temporary hack until all dependencies filter their .la files
-find $PREFIX -name '*.la' -delete
-
-./autobuild
+./autogen.sh
 ./configure --prefix=$PREFIX
 make -j$CPU_COUNT
 make install
-
-# Filter .la files: See https://github.com/conda-forge/conda-forge.github.io/issues/621
-# Also: https://github.com/conda/conda-build/pull/3102
-find $PREFIX -name '*.la' -delete
