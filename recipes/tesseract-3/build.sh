4ed1f1a7deb5c551f7e4a5cc439027cef1b5d732
Matt Swain <m.swain@me.com>
2019-04-11 21:52:26 +0100

    tesseract-3: New recipe

---
+++ recipes/tesseract-3/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pushd tesseract
+
+autoreconf -fi
+LIBLEPT_HEADERSDIR=$PREFIX/include ./configure --prefix=$PREFIX --with-extra-libraries=$PREFIX/lib
+LDFLAGS="-L$PREFIX/lib" CFLAGS="-I$PREFIX/include" make -j $CPU_COUNT
+make install
+popd
+
+pushd tessdata
+cp -avf eng.traineddata $PREFIX/share/tessdata
+cp -avf osd.traineddata $PREFIX/share/tessdata
