fb72828ccdd22c4a2b69ab3e034aa163a52d9db1
Matt Swain <m.swain@me.com>
2019-05-23 23:28:21 +0100

    ocrad: Fix recipe

--- recipes/ocrad/build.sh
+++ recipes/ocrad/build.sh
@@ -4,6 +4,6 @@
 lzip -d ocrad-$PKG_VERSION.tar.lz
 tar xvf ocrad-$PKG_VERSION.tar --strip-components=1
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" AR="$AR"
 make
 make install
