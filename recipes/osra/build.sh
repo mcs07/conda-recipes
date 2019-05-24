0e56d00419036e4a2cad68999f132bb600fe273f
Matt Swain <m.swain@me.com>
2019-05-24 18:52:19 +0100

    osra: Specify include and lib directories

--- recipes/osra/build.sh
+++ recipes/osra/build.sh
@@ -1,8 +1,12 @@
 #!/bin/env bash
 
 ./configure --prefix=$PREFIX \
+    --with-gocr-include=$PREFIX/include/gocr \
+    --with-gocr-lib=$PREFIX/lib \
     --with-openbabel-include=$PREFIX/include/openbabel-2.0 \
     --with-openbabel-lib=$PREFIX/lib \
+    --with-graphicsmagick-include=$PREFIX/include \
+    --with-graphicsmagick-lib=$PREFIX/lib \
     CXXFLAGS="$CXXFLAGS -Wno-c++11-narrowing"
 make
 make install
