0bb1089f5694fc9da11325ab16a885545b9da3db
Matt Swain <m.swain@me.com>
2019-10-25 11:13:36 +0100

    openbabel: New recipe

---
+++ recipes/openbabel/bld.bat
@@ -0,0 +1,21 @@
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DWITH_INCHI=ON ^
+      -DOPENBABEL_USE_SYSTEM_INCHI=ON ^
+      -DWITH_MAEPARSER=OFF ^
+      -DRUN_SWIG=ON ^
+      -DPYTHON_BINDINGS=ON ^
+      -DPYTHON_EXECUTABLE=%PYTHON% ^
+      .
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
+
+echo d|xcopy %LIBRARY_PREFIX%\bin\data %PREFIX%\share\openbabel /e /c
+rmdir /s /q %LIBRARY_PREFIX%\bin\data
+
+xcopy %LIBRARY_PREFIX%\Lib\site-packages %PREFIX%\Lib /e /c
+rmdir /s /q %LIBRARY_PREFIX%\Lib\site-packages
