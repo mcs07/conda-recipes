b60b1b370a699b88c921cb0d72acf9129aba8076
Matt Swain <m.swain@me.com>
2019-10-26 17:52:19 +0100

    openbabel: Add back WITH_MAEPARSER=OFF

--- recipes/openbabel/bld.bat
+++ recipes/openbabel/bld.bat
@@ -4,6 +4,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DWITH_INCHI=ON ^
       -DOPENBABEL_USE_SYSTEM_INCHI=ON ^
+      -DWITH_MAEPARSER=OFF ^
       -DRUN_SWIG=ON ^
       -DPYTHON_BINDINGS=ON ^
       -DPYTHON_EXECUTABLE=%PYTHON% ^
